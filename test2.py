bcb209a1806f4becf047e6374ac39d0d0f11aede
rayunacute <120047112+rayunacute@users.noreply.github.com>
2024-08-29 14:18:48 +0200

    new file test2

---
+++ test2.py
@@ -0,0 +1,12 @@
+import pytest
+
+def factorial(n):
+    """
+    Computes the factorial of n.
+    """
+    if n < 0:
+        raise ValueError('received negative input')
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
